cfa8bf09327fb9608ea12c4e7478e43533008303
Mederick <mederick.waxin@hotmail.com>
2023-04-16 09:05:48 +0200

    Init Post Model

---
+++ database/migrations/2023_04_13_142219_create_posts_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->longText('content');
+            $table->string('cover_name');
+            $table->boolean('is_highlighted');
+            $table->unsignedInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->unsignedInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
