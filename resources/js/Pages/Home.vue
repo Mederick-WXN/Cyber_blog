0ea1b03b6827678419d2cdbe0e35b2ebf16cd916
Mederick <mederick.waxin@hotmail.com>
2023-04-16 09:05:07 +0200

    Update Home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -21,20 +21,21 @@ library.add(faUserSecret,faBookSkull)
         <section class="bg-black">
             <div class="flex flex-col items-center">
                 <ApplicationLogo class="w-64" />
-                <h1 class="mb-2 text-5xl font-extrabold text-gray-500 dark:text-neutral-200">0xCyb3r</h1>
+                <h1 class="mb-2 text-5xl font-extrabold text-white">0xCyb3r</h1>
                 <h2 class="text-xl font-thin text-gray-300">La cybersécurité vue par un cyber-étudiant : analyse, apprentissage et partage</h2>
 
             </div>
         </section>
 
         <section class="p-8 sm:p-16 text-gray-100">
+            <h2 class="text-4xl text-white">Posts récent</h2>
             <div class="container p-6 mx-auto space-y-8">
                 <a href="#" class="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-900">
                     <img src="" alt="" class="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-500">
                     <div class="p-6 space-y-2 lg:col-span-5">
                         <div>
                             <a ref="noopener noreferrer" href="#" class="py-2 text-xs tracking-wider uppercase hover:underline text-violet-400">Actu</a>
-                            <h3 class="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">La cybersécurité, par où on commence ? </h3>
+                            <h3 class="py-2 text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">La cybersécurité, par où on commence ? </h3>
                         </div>
                         <p class="pl-2 text-gray-500">Envie de devenir un ethical hacker ? Par où commencer ? Je vous livre tous les secrets pour réussir en cyber. Analyse sur les différents corps de métier au sein de la sphére offensif et defensif. Temoignage d'un ancien étudiant devenu RSSI après son école d'ingénieur.</p>
                         <div class="flex justify-between text-xs text-gray-400">
